c080c82b1dc88edbf30671b9abc61a7adcd4ecf7
Frenchfriesdock <147462218+Frenchfriesdock@users.noreply.github.com>
2025-10-20 10:21:18 +0800

    项目依赖文件的配置修改 可以保证项目可以基础的运行起来

---
+++ src/main/java/com/hosiky/carRentalSystemApplication.java
@@ -0,0 +1,12 @@
+package com.hosiky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class carRentalSystemApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(carRentalSystemApplication.class, args);
+    }
+}
